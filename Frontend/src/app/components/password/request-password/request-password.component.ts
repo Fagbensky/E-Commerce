69bfaa950f3390d76fd5e138ec4e35f63a1caa33
Joshua Fagbenro <foja.digital@gmail.com>
2021-11-07 00:21:41 +0100

    First Components Created

---
+++ Frontend/src/app/components/password/request-password/request-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-reset',
+  templateUrl: './request-password.component.html',
+  styleUrls: ['./request-password.component.scss']
+})
+export class RequestResetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
