e88a9dd1d07a8d47dcf0b58446a09e4442742589
Joshua Fagbenro <foja.digital@gmail.com>
2021-11-07 01:49:21 +0100

    Auth Router

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RequestResetComponent } from './components/password/request-password/request-password.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'password-reset', component: RequestResetComponent},
+  {path:'reset-response', component: ResponseResetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
